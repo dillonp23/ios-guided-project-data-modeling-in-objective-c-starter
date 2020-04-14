0379856f944a451145f3ea4d105ac33b1080cce7
dillonp23 <dillonp.lambda@gmail.com>
2020-04-13 19:20:35 -0700

    Added quake model class

---
+++ Quakes-Objc/Quake.h
@@ -0,0 +1,26 @@
+//
+//  Quake.h
+//  Quakes-Objc
+//
+//  Created by Dillon P on 4/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Quake : NSObject
+
+
+
+// since these are marked read only we're going to need a custom initializer to set these properties
+@property (nonatomic, readonly) double magnitude;
+@property (nonatomic, readonly, copy) NSString *place; // use * when it is an objective-c object
+@property (nonatomic) NSDate *date;
+@property (nonatomic, readonly) double latitude;
+@property (nonatomic, readonly) double longitude;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Quakes-Objc/Quake.m
@@ -0,0 +1,13 @@
+//
+//  Quake.m
+//  Quakes-Objc
+//
+//  Created by Dillon P on 4/13/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "Quake.h"
+
+@implementation Quake
+
+@end
